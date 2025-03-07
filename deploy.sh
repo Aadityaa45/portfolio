03ac7c3649f54455b9c655069f117fed85996ab5
Aadityaa45 <aaditya.joshi@avantika.edu.in>
2025-03-07 23:49:35 +0530

    Rebuilding site

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Aadityaa45/MyPortfolio.git main:gh-pages
+git push -f git@github.com:Aadityaa45/portfolio.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyPortfolio/',
+  // base: '/MyPortfolio/Hero',
+  base:'//localhost:5174/',
   plugins: [react()],
   build: {
     outDir: 'dist',
